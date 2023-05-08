3154883f3c950b3dd7a11df1752a5db3aa5a7cfe
teoser99 <57498915+teoser99@users.noreply.github.com>
2023-05-08 19:57:52 +0300

    Game nearly completed

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject sonZemin;
+
+    private void Start()
+    {
+        for (int i = 1; i < 20; i++)
+        {
+            ZeminOlustur();
+        }
+    }
+    public void ZeminOlustur()
+    {
+        Vector3 yon;
+
+        if(Random.Range(0,2) == 0)//0 gelirse x ekseninde zemin koy
+        {
+            yon = Vector3.left;
+        }
+        else//1 gelirse z eksninde zemin koy
+        {
+            yon = Vector3.back;
+        }
+        sonZemin = Instantiate(sonZemin, sonZemin.transform.position
+            + yon, sonZemin.transform.rotation);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Out Component")]
+    [SerializeField] float speed;
+    [SerializeField] Text scoreText, bestScoreText;
+    [SerializeField] GameObject restartPanel, playGamePanel;
+
+    [Header("Public Variable")]
+    public GroundSpawner groundSpawner;
+    public static bool isDead = true;
+    public float hizlanmaZorlugu;
+
+    Vector3 yon = Vector3.left;
+    int bestScore = 0;
+    float score = 0f;
+    float artisMiktari = 1f;
+    
+
+
+
+    private void Start()
+    {
+        if(RestartGame.isRestart)
+        {
+            isDead = false;
+            playGamePanel.SetActive(false);
+        }
+
+        bestScore = PlayerPrefs.GetInt("BestScore");
+        bestScoreText.text = "Best: "+ bestScore.ToString();
+    }
+
+    private void Update()
+    {
+        if(isDead)
+        {
+            return;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(yon.x==0)//z ekseninde hareket ediyor demektir
+            {
+                yon = Vector3.left;
+            }
+            else
+            {
+                yon = Vector3.back;
+            }
+        }
+
+        if(transform.position.y< 0.1f)
+        {
+            isDead = true;
+            if(bestScore<score)
+            {
+                bestScore = (int)score;
+                PlayerPrefs.SetInt("BestScore", bestScore);
+            }
+            restartPanel.SetActive(true);
+            Destroy(this.gameObject, 3f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isDead)
+        {
+            return;
+        }
+
+        Vector3 hareket = yon * speed * Time.deltaTime; //objemizin hareket değeri
+        speed += Time.deltaTime * hizlanmaZorlugu;
+        transform.position += hareket; //hareket değerini sürekli pozisyonuma ekle
+
+        score += artisMiktari * speed * Time.deltaTime;
+        scoreText.text = "Score: "+ ((int) score).ToString();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Zemin"))
+        {
+            StartCoroutine(YokEt(collision.gameObject));
+            groundSpawner.ZeminOlustur();
+        }
+    }
+
+    IEnumerator YokEt(GameObject zemin)
+    {
+        yield return new WaitForSeconds(0.2f);
+        zemin.AddComponent<Rigidbody>();
+
+        yield return new WaitForSeconds(0.4f);
+        Destroy(zemin);
+    }
+
+    public void PlayGame()
+    {
+        isDead = false;
+        playGamePanel.SetActive(false);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}//class
